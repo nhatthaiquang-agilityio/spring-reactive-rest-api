9c05f17a25e00cc1975736ee0dccf850e28b4a37
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-01-03 10:45:07 +0700

    Update Security Config

--- src/main/java/example/security/SecurityConfig.java
+++ src/main/java/example/security/SecurityConfig.java
@@ -1,32 +1,24 @@
 package example.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.User.UserBuilder;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-// @EnableWebSecurity
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    // @Bean
-	// public UserDetailsService userDetailsService() throws Exception {
-	// 	// ensure the passwords are encoded properly
-	// 	// UserBuilder users = User.withDefaultPasswordEncoder();
-	// 	// InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-	// 	// manager.createUser(users.username("user").password("password").roles("USER").build());
-	// 	// manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build());
-    //     // return manager;
-    //     UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
-    //     return new InMemoryUserDetailsManager(user);
-    // }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests().antMatchers("/**").permitAll()
+            .and().csrf().disable().httpBasic();
+    }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsRepository() {
